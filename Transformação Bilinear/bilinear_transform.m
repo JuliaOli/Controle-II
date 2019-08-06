f1b4de275f8f5a792fd69312db612703c58ae8aa
JuliaOli <maju.olivi@gmail.com>
2019-08-06 18:56:43 -0300

    Finished

--- Transformação Bilinear/bilinear_transform.m
+++ Transformação Bilinear/bilinear_transform.m
@@ -24,6 +24,14 @@ w_0 = (1-a2+a1-a0);
 
 
 %% Routh-Hurwitz
+<<<<<<< HEAD
+
+%Condicoes
+%Equ1 = w_3 >= 0
+%Equ2 = w_2 >= 0
+%Equ3 = b1 >= 0
+%Equ4 = c1 = w_0 >= 0
+=======
 % s1_matrix = [w_3,w_1; w_2,w_0];
 % s1 = det(s1_matrix)/s1_matrix
 % 
@@ -37,12 +45,12 @@ C_1 = D(w_2, w_0, B_1, 0);
 eqnT = C_1 == 0;
 solveT = solve(eqnT,T);
 
-%Vc achou esse T_MAX = 100/837 e ficou outras duas funÁıes em funÁ„o de K e T, n„o foi?
-% O que vocÍ precisa fazer depois disso È: 
+%Vc achou esse T_MAX = 100/837 e ficou outras duas funÔøΩÔøΩes em funÔøΩÔøΩo de K e T, nÔøΩo foi?
+% O que vocÔøΩ precisa fazer depois disso ÔøΩ: 
 % 
-% 1 - usar solve(eq, K) nessas equaÁıes que sobraram (ainda sem substituir T);
-% 2 - transformar o resultado dos solves em funÁıes usando a funÁ„o do matlab "matlabFunction(Solved)"
-% 3 - aplicar os valores de T nessas funÁıes acima: 0:0.0001:T_MAX
+% 1 - usar solve(eq, K) nessas equaÔøΩÔøΩes que sobraram (ainda sem substituir T);
+% 2 - transformar o resultado dos solves em funÔøΩÔøΩes usando a funÔøΩÔøΩo do matlab "matlabFunction(Solved)"
+% 3 - aplicar os valores de T nessas funÔøΩÔøΩes acima: 0:0.0001:T_MAX
 
 eqnK = B_1 == 0;
 solveK = solve(eqnK,K);
@@ -61,8 +69,54 @@ for count = 1:vector_time
     vector_B(count) = B_K(count)
 end
 
+>>>>>>> d808ed52bf35ab7c25d14b4a14158bb01e104993
 
+%Equacao generica de Routh-Hurwitz
+D = @(s1,s2,s3,s4) (-1)*((s1*s4)-(s2*s3))/s3;
 
+%% Encontrando c_0 que √© igual a w_0
+B_1 = D(w_3, w_1, w_2, w_0)
+C_1 = D(w_2, w_0, B_1, 0);
 
+Equ_4 = C_1 == 0;
 
+Equ_sol_4 = solve(Equ_4,T);
+
+T_final = Equ_sol_4;
+
+%% Encontrando w_3
+
+Equ_1 = w_3 == 0
+Equ_sol_1 = solve(Equ_1,K);
+
+%% Encontrando w_2
+
+Equ_2 = w_2 == 0;
+Equ_sol_2 = solve(Equ_2, K);
+
+%% Encontrando b1
+Equ_3 = B_1 == 0
+Equ_sol_3 = solve(Equ_3, K);
+
+%% Equations pra plotar grafico
+
+%Encontrar K em relation a T (sao dois)
+Q_1 = matlabFunction(Equ_sol_2)
+Q_2 = matlabFunction(Equ_sol_3(1)) %ignorando raizes negativas, porque n√£o d√° bom
+
+Ts = double([0:0.001:T_final])
+K_final_1 = arrayfun(Q_1, Ts)
+K_final_2 = arrayfun(Q_2, Ts)
+
+%% Cada valor de K em relacao a T
+%Valores de K para que a funcao seja est√°vel pelo crit√©rio de Routh-Hurwitz
+%T √© o sample time p/ cada valor de K √© est√°vel
+
+figure(1)
+semilogy(Ts,K_final_1, 'k')
+
+hold on
+semilogy(Ts,K_final_2, 'r')
+xlabel('T(Sample Time)')
+ylabel('K(K possible values)')
 
