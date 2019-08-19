3e24b440117d1df0c625442e210513cb2cf88534
JuliaOli <maju.olivi@gmail.com>
2019-08-19 11:02:59 -0300

    m5_lec6 finished

--- Projeto de controladores Lead-Lag/m5_lec6.m
+++ Projeto de controladores Lead-Lag/m5_lec6.m
@@ -37,6 +37,57 @@ W_G = equ_sol(2) %deu certo aqui
 phase = -90 - atan(W_G)*180/pi
 %margem de fase do sistema sem compensação para o valor de K dado. PM é 18
 margin(G*K)
+% Defining The maximum phase corresponds to sin(φmax)
+
+PM = 18;
+
+%the additional phase lead required to maintain PM=45 at  ω g = 3.1 rad/sec is 
+phi_max = 45 - PM;
+phi_max = phi_max + 10
+
+alpha = (1-sin(phi_max*180/pi))/(1+sin(phi_max*180/pi))
+
+% finding τ
+
+w_max = 4.41 %nem sei de onde veio
+
+tetazinho = 1/(w_max*(alpha)^(1/2))
+
+% Lead Compensator
+
+num = [tetazinho 1];
+den = [tetazinho*alpha 1];
+
+C = tf(num, den)
+C = K*C
+%% Example 2
+
+num = [1];
+den = [1 1 1 0];
+G_s = tf(num, den)
+margin(G_s)
+
+% num_z = [1 1];
+% den_z = [1];
+% Z = filt(num_z, den_z)
+% 
+% G_aux = c2d(G_s, 0.2)*Z
+Ts = 0.2;
+num = [0.0187 0.0175];
+den = [1 -1.8187 0.8187];
+G_z = tf(num, den, Ts)
+
+
+%margin(G_z)
+
+
+
+
+
+
+
+
+
 
 
 
